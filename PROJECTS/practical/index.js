896e8938d2d1eb92f17ff2e507e62b0af917427c
Soares <stephaniasoares1@gmail.com>
2025-02-03 16:05:22 +0100

    first commit

---
+++ PROJECTS/practical/index.js
@@ -0,0 +1,9 @@
+
+let fname="steph";
+let lname="soa";
+let age = prompt('guess my age');
+
+//let result= fname + " " +  lname + '' +  age +  'years old';
+//alert(result);
+let result = `${fname} ${lname} is ${age} years old`;
+alert(result);
